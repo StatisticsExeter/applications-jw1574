abde12e6c843fb3951858e77f7ecc56983c5bab9
Joew <jw1574@exeter.ac.uk>
2025-11-25 17:29:47 +0000

    fixed hello test

--- course/intro/tests/test_hello.py
+++ course/intro/tests/test_hello.py
@@ -6,14 +6,14 @@ def test_greet_output(capsys):
     greet()
     captured = capsys.readouterr()
     assert captured.out.strip() == "Hello, World!"
-test_greet_output()
+
 
 def test_add_two_numbers_too_simple():
     simple_sum = add_two_numbers(2, 2)
     assert simple_sum == 4
 
 
-@pytest.mark.parametrize("input_one, input_two , expected", [
+@pytest.mark.parametrize("input_one, input_two, expected", [
     (1, 2, 3),
     (4, 8, 12),
     (-1, -3, -4),
@@ -21,8 +21,3 @@ def test_add_two_numbers_too_simple():
 def test_add_two_numbers_sensible(input_one, input_two, expected):
     sensible_sum = add_two_numbers(input_one, input_two)
     assert sensible_sum == expected
-import os
-
-print(os.getcwd())
-print(os.listdir())
-os.chdir("C:/Users/JWWeb/Documents/applications-jw1574")

14f6fe78b510ae5cb24807f84b32d50d045eadb3
Joew <jw1574@exeter.ac.uk>
2025-11-27 14:52:03 +0000

    fixed prediction stuff

--- course/supervised_classification/predict.py
+++ course/supervised_classification/predict.py
@@ -30,7 +30,8 @@ def pred_lda():
     model_path = base_dir / 'data_cache' / 'models' / 'lda_model.joblib'
     X_test_path = base_dir / 'data_cache' / 'energy_X_test.csv'
     y_pred_path = base_dir / 'data_cache' / 'models' / 'lda_y_pred.csv'
-    predict(model_path, X_test_path, y_pred_path)
+    y_pred_prob_path = base_dir / 'data_cache' / 'models' / 'lda_y_pred_proba.csv'  
+    predict(model_path, X_test_path, y_pred_path, y_pred_prob_path)
 
 
 def pred_qda():
@@ -38,4 +39,5 @@ def pred_qda():
     model_path = base_dir / 'data_cache' / 'models' / 'qda_model.joblib'
     X_test_path = base_dir / 'data_cache' / 'energy_X_test.csv'
     y_pred_path = base_dir / 'data_cache' / 'models' / 'qda_y_pred.csv'
-    predict(model_path, X_test_path, y_pred_path)
+    y_pred_prob_path = base_dir / 'data_cache' / 'models' / 'qda_y_pred_proba.csv'  # new
+    predict(model_path, X_test_path, y_pred_path, y_pred_prob_path)
